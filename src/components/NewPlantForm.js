f17debdd26848d3d594b2671a0373cb121656786
Shaun <smontoya0009@gmail.com>
2025-12-06 20:59:08 -0500

    complete code challenge

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewPlantForm() {
+function NewPlantForm({ onAddPlant }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [price, setPrice] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const plantData = {
+      name,
+      image,
+      price,
+    };
+
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/JSON"
+      },
+      body: JSON.stringify(plantData)
+    })
+      .then((response) => response.json())
+      .then((item) => {
+        onAddPlant(item);
+      });
+
+    setName("");
+    setImage("");
+    setPrice("");
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={name} placeholder="Plant name" onChange={(e) => setName(e.target.value)}/>
+        <input type="text" value={image} placeholder="Image URL" onChange={(e) => setImage(e.target.value)}/>
+        <input type="number" value={price} step="0.01" placeholder="Price" onChange={(e) => setPrice(e.target.value)}/>
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,14 @@
 import React from "react";
 
-function PlantCard() {
+function PlantCard({ plant, onToggleStock }) {
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+      <button onClick={() => onToggleStock(plant.id)}>
+        {plant.soldOut ? "Out of Stock" : "In Stock"}
+      </button>
     </li>
   );
 }
